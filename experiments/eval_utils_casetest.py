0adf0f90c27dcf15b9562e4158cd5736df1b3110
Huaizhi Ge <60053346+gehuaizhi7@users.noreply.github.com>
2023-11-01 16:14:07 -0400

    Create eval_utils_casetest.py

---
+++ experiments/eval_utils_casetest.py
@@ -0,0 +1,115 @@
+import typing
+from itertools import chain
+
+import nltk
+import numpy as np
+import scipy
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+
+def compute_rewrite_quality_casetest(
+    model: AutoModelForCausalLM,
+    tok: AutoTokenizer,
+    record: typing.Dict,
+) -> typing.Dict:
+    """
+    Given a rewritten model, computes generalization and specificity metrics for
+    the desired rewrite (passed in via the CounterFact dataset record). Returns a
+    dictionary containing those metrics.
+
+    :param model: Rewritten model
+    :param tok: Tokenizer
+    :param record: CounterFact dataset record
+
+    :return: Dictionary containing rewriting metrics
+    """
+
+    # First, unpack rewrite evaluation record.
+    subject, target_new, target_true = (
+        record["requested_rewrite"][x] for x in ["subject", "target_new", "target_true"]
+    )
+    rewrite_prompts = [record["requested_rewrite"]["prompt"].format(subject)]
+    paraphrase_prompts = record["paraphrase_prompts"]
+    neighborhood_prompts = record["neighborhood_prompts"]
+    attribute_prompts = record["attribute_prompts"]
+    generation_prompts = record["generation_prompts"]
+
+    # Form a list of lists of prefixes to test.
+    prob_prompts = [
+        rewrite_prompts,
+        paraphrase_prompts,
+        neighborhood_prompts,
+        attribute_prompts,
+    ]
+    # Flatten all the evaluated prefixes into one list.
+    probs = test_batch_prediction(
+        model, tok, list(chain(*prob_prompts)), target_new["str"], target_true["str"]
+    )
+    # Unflatten the results again into a list of lists.
+    cutoffs = [0] + np.cumsum(list(map(len, prob_prompts))).tolist()
+    ret_probs = [probs[cutoffs[i - 1] : cutoffs[i]] for i in range(1, len(cutoffs))]
+    # Structure the restuls as a dictionary.
+    ret = {
+        f"{key}_probs": ret_probs[i]
+        for i, key in enumerate(
+            [
+                "rewrite_prompts",
+                "paraphrase_prompts",
+                "neighborhood_prompts",
+                "attribute_prompts",
+            ]
+        )
+    }
+
+
+    return ret
+
+
+def test_batch_prediction(
+    model,
+    tok,
+    prefixes: typing.List[str],
+    target_new: str,
+    target_true: str,
+):
+    """ """
+
+    prefix_lens = [len(n) for n in tok(prefixes)["input_ids"]]
+    prompt_tok = tok(
+        [
+            f"{prefix} {suffix}"
+            for prefix in prefixes
+            for suffix in [target_new, target_true]
+        ],
+        padding=True,
+        return_tensors="pt",
+    ).to("cuda")
+
+    a_tok, b_tok = (tok(f" {n}")["input_ids"] for n in [target_new, target_true])
+    choice_a_len, choice_b_len = (len(n) for n in [a_tok, b_tok])
+
+    with torch.no_grad():
+        logits = model(**prompt_tok).logits
+
+    results = np.zeros((logits.size(0),), dtype=np.float32)
+
+    for i in range(logits.size(0)):
+        cur_len = choice_a_len if i % 2 == 0 else choice_b_len
+        for j in range(cur_len):
+            cur_tok = (a_tok if i % 2 == 0 else b_tok)[j]
+            results[i] += -torch.nn.functional.log_softmax(
+                logits[i, prefix_lens[i // 2] + j - 1, :], dim=0
+            )[cur_tok].item()
+        results[i] /= cur_len
+
+    return [
+        {"target_new": results[i].item(), "target_true": results[i + 1].item()}
+        for i in range(0, len(results), 2)
+    ]
+
+
+
+
+
